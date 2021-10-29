08ea5707da1a39e52a1411a4f398fa897660ac6a
KrishnadasPC <pckrishnadas88@gmail.com>
2021-10-29 15:32:25 +0530

    init pupetter sample pdf repo.

---
+++ index.js
@@ -0,0 +1,20 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  console.time('Time Taken');
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setViewport({
+    width: 1600,
+    height: 1200,
+    deviceScaleFactor:1
+  });
+  await page.goto('http://localhost:3000', {
+    waitUntil: 'networkidle2',
+  });
+  await page.pdf({ path: 'sample-file.pdf'});
+
+  await browser.close();
+  console.timeEnd('Time Taken');
+})();
+
